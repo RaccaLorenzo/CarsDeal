4af55bda82407f97fc4ef51ff159c479cc720cd6
mirko <m.minni.1764@vallauri.edu>
2024-02-22 19:15:35 +0100

    login component - responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavbarComponent]
+    imports: [RouterOutlet, NavbarComponent, LoginFormComponent]
 })
 export class AppComponent {
   title = 'CarsDeal';
