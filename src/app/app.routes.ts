f39119c19ea255a5c63a6ebac570217b9288503d
Mirko <m.minni.1764@vallauri.edu>
2024-03-02 09:33:10 +0100

    form ricerca - component home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {path : '', redirectTo: '/login', pathMatch: 'full'},
+    {path : '', redirectTo: '/home', pathMatch: 'full'},
     {path : 'login', component: LoginFormComponent},
     {path : 'registration', component: RegistrationFormComponent},
+    {path : 'home', component: HomeComponent}
 ];
