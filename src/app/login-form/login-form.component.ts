0608928775574b41be48c081b5a17076f3de7767
Mirko <m.minni.1764@vallauri.edu>
2024-03-14 11:38:54 +0100

    da fixare ngModel

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core'; 
-import { Router } from '@angular/router'; 
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
@@ -10,13 +10,34 @@ import { Router } from '@angular/router';
 })
 export class LoginFormComponent {
 
-  router : Router;
-  constructor(router: Router) {
+  url : string = 'http://10.0.100.153:3000/';
+  router: Router;
+  pwd: string = '';
+  constructor(router: Router, ) {
     this.router = router; // Assign the injected router parameter to the class property.
   }
 
-  redirectRegistrazione(){
+  redirectRegistrazione() {
     this.router.navigate(['/registration']);
     console.log("Reindirizzamento a registrazione effettuato con successo");
   }
+
+  login() {
+    fetch(`${this.url}login`, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: ``,
+        password: ``
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response: any) => {
+        console.log(response.data);
+      })
+      .catch((error: any) => {
+        console.error(error);
+      });
+  };
 }
